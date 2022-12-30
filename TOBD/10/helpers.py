4967785739eed5bb0f3a533de1b75f63c28890a5
Borisov Nikita <nikitaborisov1594@yandex.ru>
2022-12-30 15:16:08 +0300

    RITP

--- TOBD/10/helpers.py
+++ TOBD/10/helpers.py
@@ -1,7 +1,6 @@
-
 import pandas as pd
-
 def get_tag_sum_count_from_file(file: str) -> dict:
     df = pd.read_csv(file)
-    df2 = df.groupby("tags")["n_steps"].agg(sum = 'sum', count = 'count')
-    return df2.to_dict('index')
+    df2 = df.groupby("tags")["n_steps"]
+    df3 = df2.agg(sum = 'sum', count = 'count')
+    return df3.to_dict('index')
--- TOBD/10/helpers2.py
+++ TOBD/10/helpers2.py
@@ -2,5 +2,6 @@ import pandas as pd
 
 def get_tag_sum_count_from_file(file: str, output) -> dict:
     df = pd.read_csv(file)
-    df2 = df.groupby("tags")["n_steps"].agg(sum = 'sum', count = 'count')
-    output.put(df2.to_dict('index'))
+    df2 = df.groupby("tags")["n_steps"]
+    df3 = df2.agg(sum = 'sum', count = 'count')
+    output.put(df3.to_dict('index'))
